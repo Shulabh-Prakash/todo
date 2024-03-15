17ebd9efb618b62f40b5a2d38cbf28f4affeac69
Shulabh Prakash <prakashshulabh@gmail.com>
2024-03-15 18:43:00 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import {useState} from "react";
+import WeclomeMessage from "./components/WelcomeMessage";
+function App() {
+
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [...todoItems, {name: itemName, dueDate: itemDueDate}];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter(item => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  }
+
+  return  <center className='todo-container'>
+    <AppName />
+  <div className="container text-center">
+  <AddTodo onNewItem={handleNewItem}/>
+  {todoItems.length=== 0 && <WeclomeMessage></WeclomeMessage>}
+  <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem}></TodoItems>
+  
+  
+   
+</div>
+  </center>
+}
+
+export default App
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,12 @@
+function TodoItem({todoName, todoDate, onDeleteClick}) {
+
+    return   <div className="row s-row">
+    <div className="col-6">{todoName}</div>
+    <div className="col-4">{todoDate}</div>
+    <div className="col-2">
+    <button type="button" className="btn btn-danger s-button" onClick={() => onDeleteClick(todoName)}>Delete</button>
+    </div>
+  </div>;
+}
+
+export default TodoItem;
